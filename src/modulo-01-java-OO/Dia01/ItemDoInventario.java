59ba8939984cebbec39eef4cd1ab5dffd2fd817e
Augusto Schiller Wartchow <augusto.wartchow@gmail.com>
2015-04-07 17:29:40 -0300

    Criação da Classe ItemDoInventario e sua classe de Testes;

---
+++ src/modulo-01-java-OO/Dia01/ItemDoInventario.java
@@ -0,0 +1,27 @@
+
+/**
+ * Classe para identificar os itens que poderão ser utlizados
+ * 
+ * @author Augusto Schiller
+ */
+public class ItemDoInventario
+{
+   private String descricao;
+   private int quantidade;
+   
+   public ItemDoInventario(String desc, int qtd)
+   {
+       descricao = desc;
+       quantidade = qtd;
+   }
+   
+   //GETs
+   public String getDescricao()
+   {
+       return descricao;
+   }
+   public int getQuantidade()
+   {
+       return quantidade;
+   }
+}
---
+++ src/modulo-01-java-OO/Dia01/ItemDoInventarioTest.java
@@ -0,0 +1,38 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Classe de testes unitários para a Classe ItemDoInventário.
+ *
+ * @author Augusto Schiller
+ */
+public class ItemDoInventarioTest
+{
+    @Test
+    public void criaItemComDescricaoEQuantidade()
+    {
+        ItemDoInventario item = new ItemDoInventario("Teste de Descrição", 12);
+        
+        String descricaoEsperada = "Teste de Descrição";
+        int quantidadeEsperada = 12;
+        
+        assertEquals(descricaoEsperada, item.getDescricao());
+        assertEquals(quantidadeEsperada, item.getQuantidade());        
+    }
+    
+    @Test
+    public void criaItemComDescricaoNulaEQuantidadeNegativa()
+    {
+        ItemDoInventario item = new ItemDoInventario(null, -12);
+        
+        String descricaoEsperada = null;
+        int quantidadeEsperada = -12;
+        
+        assertEquals(descricaoEsperada, item.getDescricao());
+        assertEquals(quantidadeEsperada, item.getQuantidade());        
+    }
+}
