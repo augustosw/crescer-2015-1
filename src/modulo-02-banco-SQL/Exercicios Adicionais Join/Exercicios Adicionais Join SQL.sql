1b484e86a50db457e38f5dffc2c41fcb6bf0d4da
Augusto Schiller Wartchow <augusto.wartchow@gmail.com>
2015-04-27 17:43:52 -0300

    Alteração de exercicios adicionais JOIN SQL

--- src/modulo-02-banco-SQL/Exercicios Adicionais Join/Exercicios Adicionais Join SQL.sql
+++ src/modulo-02-banco-SQL/Exercicios Adicionais Join/Exercicios Adicionais Join SQL.sql
@@ -1,31 +1,63 @@
-begin transaction
+--1) Identifique e liste quantos valores diferentes estão definidos para a coluna 
+--   Situacao da tabela Produto (somente os distintos valores).
+select Situacao, count (1) as Quantidade
+from Produto
+group by Situacao
+--2) Liste todos os clientes que tenham o LTDA no nome ou razao social.
+select Nome, RazaoSocial 
+from Cliente 
+Where Nome LIKE '%Ltda%' OR RazaoSocial LIKE '%Ltda%'
 
---3.1) Garantir que a situação tenha somente os valores A ou I.
-ALTER TABLE Cliente 
-ADD  CONSTRAINT CK_Situacao
-CHECK (Situacao in ('A', 'I'));
-commit
 
---3.2)(Pedido) Nenhum pedido poderá ter a data de entrega menor que a data do pedido.
+--3) Crie (insira) um novo registro na tabela de Produto, com as seguintes informações:
+--   Nome: Galocha Maragato
+--   Preço de custo: 35.67
+--   Preço de venda: 77.95
+--   Situação: A
 Begin transaction
-ALTER TABLE Pedido
-ADD CONSTRAINT CK_DataPedido_Maior_Que_DataEntrega
-CHECK (DataPedido < DataEntrega);
+insert into Produto(Nome, DataCadastro, PrecoCusto, PrecoVenda, Situacao) 
+values('Galocha Maragato', GETDATE(), 35.67, 77.95, 'A')
 commit
 
---3.3)(Pedido) Garantir que a situação tenha somente os seguintes valores: A, C, Q e I.
-begin transaction
-ALTER TABLE Pedido
-ADD  CONSTRAINT CK_Situacao_Pedido
-CHECK (Situacao in ('A', 'C', 'Q', 'I'));
-commit
+select * from Produto
+where Nome like 'Galocha Maragato'
 
---3.4)(PeditoItem) A quantidade não pode ser igual ou menor que zero.
-begin transaction
-ALTER TABLE PedidoItem
-ADD CONSTRAINT CK_Quantidade_Maior_Que_Zero
-CHECK (Quantidade > 0)
+-- 4) Identifique e liste os produtos que não tiveram nenhum pedido, 
+--    considere os relacionamentos no modelo de dados, pois não há relacionamento 
+--    direto entre Produto e Pedido (será preciso relacionar PedidoItem).
+--    Obs.: o produto criado anteriormente deverá ser listado.
+Select IDProduto, Nome, Situacao
+From Produto
+Where IDProduto not in(select P.IDProduto  
+					   from Produto P, PedidoItem Pitem
+					   where P.IDProduto = Pitem.IDProduto)
+
+
+--5) Identifique qual o estado (coluna UF da tabela Cidade) possuí o maior 
+--   número de clientes (tabela Cliente), liste também qual o Estado possuí o menor número de clientes.
+--   Dica: pode (não é obrigatório) ser utilizado subquery, e também UNION.
+select * from MaiorCidade
+Union all
+select * from MenorCidade
+
+Create view MaiorCidade as
+select top 1 C.UF, Count(1) as Quantidade
+from Cliente Cli, Cidade C
+where Cli.IDCidade = C.IDCidade
+group by C.UF
+Order by Quantidade desc
+
+Create view MenorCidade as
+select top 1 C.UF, Count(1) as Quantidade
+from Cliente Cli, Cidade C
+where Cli.IDCidade = C.IDCidade
+group by C.UF
+Order by Quantidade
 
 
 
---3.5)(PeditoItem) Garantir que a situação tenha somente os seguintes valores: A, C, Q e I.
+--6) Liste o total de cidades (distintas) que possuem clientes que realizaram algum pedido.
+--   Dica: será preciso relacionar Cidade com Cliente, e Cliente com Pedido.
+Select distinct Ci.Nome 
+from Cliente C INNER JOIN Pedido P ON C.IDCliente = P.IDCliente
+		      INNER JOIN Cidade Ci ON C.IDCidade = Ci.IDCidade
