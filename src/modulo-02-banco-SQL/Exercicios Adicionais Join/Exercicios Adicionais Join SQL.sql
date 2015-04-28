11cb8848b7d9e33f6acf941b18bc5f403860f719
Augusto Schiller Wartchow <augusto.wartchow@gmail.com>
2015-04-28 15:57:44 -0300

    Correção Exercicios Adicionais JOIN SQL - 1 a 6

--- src/modulo-02-banco-SQL/Exercicios Adicionais Join/Exercicios Adicionais Join SQL.sql
+++ src/modulo-02-banco-SQL/Exercicios Adicionais Join/Exercicios Adicionais Join SQL.sql
@@ -32,6 +32,7 @@ Where IDProduto not in(select P.IDProduto
 					   from Produto P, PedidoItem Pitem
 					   where P.IDProduto = Pitem.IDProduto)
 
+CREATE INDEX IX_Pedido_Produto ON PedidoItem (IDProduto);
 
 --5) Identifique qual o estado (coluna UF da tabela Cidade) possuí o maior 
 --   número de clientes (tabela Cliente), liste também qual o Estado possuí o menor número de clientes.
@@ -55,9 +56,50 @@ group by C.UF
 Order by Quantidade
 
 
+create index IX_Cidade_Cliente ON Cliente(IDCidade);
+
 
 --6) Liste o total de cidades (distintas) que possuem clientes que realizaram algum pedido.
 --   Dica: será preciso relacionar Cidade com Cliente, e Cliente com Pedido.
-Select distinct Ci.Nome 
+Select count(distinct Ci.Nome) as TotalCidades
 from Cliente C INNER JOIN Pedido P ON C.IDCliente = P.IDCliente
 		      INNER JOIN Cidade Ci ON C.IDCidade = Ci.IDCidade
+
+
+select Count(distinct Nome) as TotalCidades
+From Cidade
+Where EXISTS (Select 1
+			  From Cliente Cli
+				   inner join Pedido Ped on Ped.IDCliente = Cli.IDCliente
+			  Where Cli.IDCidade = Cidade.IDCidade)
+
+--7) Dentro da atual estrutura, cada produto é composto por 1 ou vários materiais 
+--   (tabela ProdutoMaterial). Identifique se existe algum produto sem material relacionado.
+--   Obs.: o produto criado anteriormente deverá ser listado.
+
+
+
+--8) Liste os produtos, com seu preço de custo, e relacione com seus os materiais 
+--   (ProdutoMaterial), e liste também o somatório do PrecoCusto de todos seus materiais. 
+--   Veja um exemplo abaixo: IDProduto    Produto     Preco Custo R$     Preco Custo Materiais R$
+--                            1589      Chin Palm3940        883,74                1.092,02
+--                            2253    Salg Plops Presunto    370,88                  823,03
+--                            3581   Garrafa Madri Rolha     872,43                1.469,31
+
+
+--9) Após identificar o preço de custo dos produtos e seus materiais será preciso acertar os
+-- produtos que estão com o valor de custo inferior ao custo dos materiais. 
+-- Pra isso faça uma alteração (update) na tabela de Produtos, definindo o PrecoCusto, 
+-- para que fique igual ao custo dos seus materiais.
+
+
+
+-- 10) Liste os clientes que tenham o mesmo nome (Tabela Cliente, registros com o nome (apenas) duplicado).
+
+
+
+--11) Lista qual o primeiro nome mais popular entre os clientes, considere apenas o primeiro nome.
+
+
+
+--12) Liste qual o produto é mais vendido (considere a informação da quantidade).
